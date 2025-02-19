f68045ca6a3f584763038736267d3057d9a5b91d
Md Shahriar Hossain <shahriarhu@gmail.com>
2025-02-19 10:24:16 +0600

    updated theme

--- lib/app/ui/theme/theme.dart
+++ lib/app/ui/theme/theme.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:getx_architecture/app/ui/theme/colors.dart';
-import 'package:getx_architecture/app/utils/ui_spacing.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+/// Light Theme
 ThemeData lightTheme = ThemeData.light().copyWith(
   primaryColor: primaryColor,
   primaryColorLight: primaryColorLight,
@@ -13,6 +14,10 @@ ThemeData lightTheme = ThemeData.light().copyWith(
   iconTheme: const IconThemeData(color: lightIconColor),
   primaryIconTheme: const IconThemeData(color: lightIconColor),
   appBarTheme: _appBarTheme(lightScaffoldColor, lightPrimaryTextColor),
+  inputDecorationTheme: _inputDecorationTheme(
+    lightDisabledTextColor,
+    lightDividerColor,
+  ),
   floatingActionButtonTheme: _floatingActionButtonTheme,
   elevatedButtonTheme: _elevatedButtonThemeData,
   textButtonTheme: _textButtonThemeData,
@@ -23,11 +28,13 @@ ThemeData lightTheme = ThemeData.light().copyWith(
     elevation: 0,
     shape: CircularNotchedRectangle(),
   ),
+  dividerTheme: const DividerThemeData(color: lightDividerColor),
   colorScheme: _lightColorScheme,
-  textTheme: _textTheme(lightPrimaryTextColor),
+  textTheme: _textTheme(lightPrimaryTextColor, lightSecondaryTextColor),
   brightness: Brightness.light,
 );
 
+/// Dark Theme
 ThemeData darkTheme = ThemeData.dark().copyWith(
   primaryColor: primaryColor,
   primaryColorLight: primaryColorLight,
@@ -39,6 +46,10 @@ ThemeData darkTheme = ThemeData.dark().copyWith(
   iconTheme: const IconThemeData(color: darkIconColor),
   primaryIconTheme: const IconThemeData(color: darkIconColor),
   appBarTheme: _appBarTheme(darkScaffoldColor, darkPrimaryTextColor),
+  inputDecorationTheme: _inputDecorationTheme(
+    darkDisabledTextColor,
+    darkDividerColor,
+  ),
   floatingActionButtonTheme: _floatingActionButtonTheme,
   elevatedButtonTheme: _elevatedButtonThemeData,
   textButtonTheme: _textButtonThemeData,
@@ -49,27 +60,83 @@ ThemeData darkTheme = ThemeData.dark().copyWith(
     elevation: 0,
     shape: CircularNotchedRectangle(),
   ),
+  dividerTheme: const DividerThemeData(color: darkDividerColor),
   colorScheme: _darkColorScheme,
-  textTheme: _textTheme(darkPrimaryTextColor),
+  textTheme: _textTheme(darkPrimaryTextColor, darkSecondaryTextColor),
   brightness: Brightness.dark,
 );
 
-TextTheme _textTheme(Color color) => TextTheme(
-      bodySmall: TextStyle(color: color),
-      bodyMedium: TextStyle(color: color),
-      bodyLarge: TextStyle(color: color),
-      titleSmall: TextStyle(color: color),
-      titleMedium: TextStyle(color: color),
-      titleLarge: TextStyle(color: color),
-      headlineSmall: TextStyle(color: color),
-      headlineMedium: TextStyle(color: color),
-      headlineLarge: TextStyle(color: color),
+/// Text Theme
+TextTheme _textTheme(Color primaryColor, Color secondaryColor) => TextTheme(
+      bodySmall: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: secondaryColor,
+        fontWeight: FontWeight.w500,
+        fontSize: 12,
+        height: 1.4,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: secondaryColor,
+        fontWeight: FontWeight.w500,
+        fontSize: 14,
+        height: 1.4,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: secondaryColor,
+        fontWeight: FontWeight.w500,
+        fontSize: 16,
+        height: 1.4,
+      ),
+      titleSmall: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: primaryColor,
+        fontWeight: FontWeight.w700,
+        fontSize: 12,
+        height: 1.4,
+      ),
+      titleMedium: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: primaryColor,
+        fontWeight: FontWeight.w700,
+        fontSize: 14,
+        height: 1.4,
+      ),
+      titleLarge: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: primaryColor,
+        fontWeight: FontWeight.w700,
+        fontSize: 16,
+        height: 1.4,
+      ),
+      headlineSmall: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: primaryColor,
+        fontWeight: FontWeight.w700,
+        fontSize: 20,
+        height: 1.3,
+      ),
+      headlineMedium: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: primaryColor,
+        fontWeight: FontWeight.w700,
+        fontSize: 24,
+        height: 1.3,
+      ),
+      headlineLarge: TextStyle(
+        fontFamily: GoogleFonts.inter().fontFamily,
+        color: primaryColor,
+        fontWeight: FontWeight.w800,
+        fontSize: 28,
+        height: 1.3,
+      ),
     );
 
 ColorScheme _lightColorScheme = const ColorScheme.light().copyWith(
   primary: primaryColor,
   secondary: secondaryColor,
-  tertiary: secondaryColor, // Adjust as needed
+  tertiary: secondaryColor,
   error: errorColor,
   surface: lightCanvasColor,
   brightness: Brightness.light,
@@ -78,12 +145,13 @@ ColorScheme _lightColorScheme = const ColorScheme.light().copyWith(
 ColorScheme _darkColorScheme = const ColorScheme.dark().copyWith(
   primary: primaryColor,
   secondary: secondaryColor,
-  tertiary: secondaryColor, // Adjust as needed
+  tertiary: secondaryColor,
   error: errorColor,
   surface: darkCanvasColor,
   brightness: Brightness.dark,
 );
 
+/// Appbar Theme
 AppBarTheme _appBarTheme(Color backgroundColor, Color foregroundColor) => AppBarTheme(
       backgroundColor: backgroundColor,
       foregroundColor: foregroundColor,
@@ -91,11 +159,33 @@ AppBarTheme _appBarTheme(Color backgroundColor, Color foregroundColor) => AppBar
       scrolledUnderElevation: 0,
     );
 
+/// InputDecoration Theme
+InputDecorationTheme _inputDecorationTheme(Color fontColor, Color borderColor) =>
+    InputDecorationTheme(
+      hintStyle: TextStyle(
+        color: fontColor,
+        fontWeight: FontWeight.w500,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(width: 1.4),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(
+          color: borderColor,
+          width: 1.4,
+        ),
+      ),
+    );
+
+/// Floating Action Button Theme
 const FloatingActionButtonThemeData _floatingActionButtonTheme = FloatingActionButtonThemeData(
   foregroundColor: Colors.white,
   backgroundColor: secondaryColor,
 );
 
+/// Elevated Button Theme
 ElevatedButtonThemeData _elevatedButtonThemeData = ElevatedButtonThemeData(
   style: ElevatedButton.styleFrom(
     foregroundColor: Colors.white,
@@ -107,11 +197,12 @@ ElevatedButtonThemeData _elevatedButtonThemeData = ElevatedButtonThemeData(
       fontSize: 18,
     ),
     shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(UISpacing.xSmall),
+      borderRadius: BorderRadius.circular(8),
     ),
   ),
 );
 
+/// Text Button Theme
 TextButtonThemeData _textButtonThemeData = TextButtonThemeData(
   style: TextButton.styleFrom(
     textStyle: const TextStyle(
@@ -119,11 +210,12 @@ TextButtonThemeData _textButtonThemeData = TextButtonThemeData(
       fontSize: 18,
     ),
     shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(UISpacing.xLarge),
+      borderRadius: BorderRadius.circular(8),
     ),
   ),
 );
 
+/// Icon Button Theme
 IconButtonThemeData _iconButtonThemeData = IconButtonThemeData(
   style: IconButton.styleFrom(
     foregroundColor: primaryColor,
@@ -132,6 +224,7 @@ IconButtonThemeData _iconButtonThemeData = IconButtonThemeData(
   ),
 );
 
+/// Page Transition Theme (Navigation)
 const PageTransitionsTheme _pageTransitionsTheme = PageTransitionsTheme(
   builders: {
     TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
